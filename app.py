a92db3c88b6992e976270011dec79a3f8f5b7cc2
swj8905 <62337255+swj8905@users.noreply.github.com>
2023-08-20 11:46:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import pydeck as pdk
 import streamlit as st
 
-st.write("# 따릉이 시각화")
+st.write("# 따릉이 실시간 시각화")
 
 api_key = "757766614b74616c374a46696a55"
 bike_dict = {"rackTotCnt":[], "stationName":[],
